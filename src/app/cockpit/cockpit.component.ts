dd40bc385d748de6fc6ce5d89546cff5cc2fe9c5
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-05-02 00:55:53 -0500

    implemented all angular databinding methods

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent:string}>();
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverCreated.emit({serverName: serverName.value, serverContent: this.serverContentInput.nativeElement.value})
+  }
+
+  onAddBlueprint(serverName: HTMLInputElement) {
+    this.blueprintCreated.emit({blueprintName: serverName.value, blueprintContent: this.serverContentInput.nativeElement.value})
+  }
+
+}
