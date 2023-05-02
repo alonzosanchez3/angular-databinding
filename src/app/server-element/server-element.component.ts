11ec4db7e98540929129c47f135a10a795d2f83b
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-05-02 01:14:16 -0500

    impleted angular lifecycle methods

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,55 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy {
+
+  @ViewChild('header', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef
 
   @Input() element: {type: string, name: string, content: string};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called')
   }
 
+  ngOnInit(): void {
+    console.log('onInit called')
+    console.log(this.header.nativeElement.textContent)
+    console.log(this.paragraph.nativeElement.textContent)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called')
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called')
+    console.log(this.header.nativeElement.textContent)
+    console.log(this.paragraph.nativeElement.textContent)
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called')
+  }
 }
+
